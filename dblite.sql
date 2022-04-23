7f558870d530e523fc0d67c36b98d8e7ed110715
Dornith <62300204+Dornith@users.noreply.github.com>
2022-04-23 20:03:26 +0100

    db script fixed

--- dblite.sql
+++ dblite.sql
@@ -1,89 +1,99 @@
-PRAGMA foreign_keys = ON;
 
-CREATE TABLE "user"
+DROP TABLE IF EXISTS Order_list;
+DROP TABLE IF EXISTS Order_row;
+DROP TABLE IF EXISTS Driver;
+DROP TABLE IF EXISTS Favorite;
+DROP TABLE IF EXISTS Review;
+DROP TABLE IF EXISTS Dish;
+DROP TABLE IF EXISTS Restaurant;
+DROP TABLE IF EXISTS Owner;
+DROP TABLE IF EXISTS User;
+
+
+CREATE TABLE User
 (
-    [Id] INTEGER PRIMARY KEY AUTOINCREMENT,
-    [Name] NVARCHAR(30) NOT NULL,
-    [Password] NVARCHAR() NOT NULL,
-    [Address] NVARCHAR(30),
-    [PhoneNumber] NVARCHAR(20)
+    UserId INTEGER NOT NULL,
+    Name NVARCHAR(30) NOT NULL,
+    Password NVARCHAR(30) NOT NULL,
+    Address NVARCHAR(30),
+    PhoneNumber NVARCHAR(20),
+    CONSTRAINT PK_User PRIMARY KEY (UserId)
 );
 
-CREATE TABLE "owner"
+CREATE TABLE Owner
 (
-    [Id_owner] INTEGER NOT NULL, 
-    FOREIGN KEY([Id_owner]) REFERENCES "user" ([Id])
+    OwnerId INTEGER NOT NULL, 
+    FOREIGN KEY(OwnerId) REFERENCES User (UserId)
 );
 
-CREATE TABLE "restaurant"
+CREATE TABLE Restaurant
 (
-    [Id] INTEGER PRIMARY KEY AUTOINCREMENT,
-    [Name] NVARCHAR() NOT NULL,
-    [Address] NVARCHAR(30) NOT NULL,
-    [Category] NVARCHAR(20) NOT NULL,
-    [Id_owner] INTEGER,
-    FOREIGN KEY([Id_owner]) REFERENCES "owner" ([Id_owner])
+    RestaurantId INTEGER NOT NULL,
+    Name NVARCHAR(30) NOT NULL,
+    Address NVARCHAR(30) NOT NULL,
+    Category NVARCHAR(20) NOT NULL,
+    Id_owner INTEGER,
+    CONSTRAINT PK_Restaurant PRIMARY KEY (RestaurantId),
+    FOREIGN KEY(Id_owner) REFERENCES Owner (OwnerId)
 );
-CREATE TABLE "dish"
+CREATE TABLE Dish
 (
-    [Id] INTEGER PRIMARY KEY AUTOINCREMENT,
-    [Name] NVARCHAR() NOT NULL,
-    [Description] NVARCHAR(),
-    [Price] REAL,
-    [Category] NVARCHAR(),
-    [Picture] NVARCHAR(),
-    [Id_restaurant] INTEGER,
-    [Promotion] REAL DEFAULT 1.0,
-    FOREIGN KEY([Id_restaurant]) REFERENCES "restaurant" ([Id])
-);
-
-CREATE TABLE "review"
-(
-    [Id] INTEGER PRIMARY KEY AUTOINCREMENT,
-    [Score] REAL NOT NULL,
-    [Description] NVARCHAR(),
-    [Picture] NVARCHAR(),
-    [Id_restaurant] INTEGER,
-    [Id_user] INTEGER,
-    FOREIGN KEY([Id_restaurant]) REFERENCES "restaurant" ([Id]),
-    FOREIGN KEY([Id_user]) REFERENCES "user"([Id])
+    DishId INTEGER NOT NULL,
+    Name NVARCHAR(30) NOT NULL,
+    Description NVARCHAR(30),
+    Price REAL,
+    Category NVARCHAR(30),
+    Picture NVARCHAR(200),
+    Id_restaurant INTEGER,
+    Promotion REAL DEFAULT 1.0,
+    CONSTRAINT PK_Dish PRIMARY KEY (DishId),
+    FOREIGN KEY(Id_restaurant) REFERENCES Restaurant (RestaurantId)
 );
 
-CREATE TABLE "favorite"
+CREATE TABLE Review
 (
-    [Id_user] INTEGER,
-    [Id_dish] INTEGER,
-    PRIMARY KEY(Id_user,Id_dish),
-    FOREIGN KEY([Id_user]) REFERENCES "user" ([Id]),
-    FOREIGN KEY([Id_dish]) REFERENCES "dish" ([Id])
+    ReviewId INTEGER NOT NULL,
+    Score REAL NOT NULL,
+    Description NVARCHAR(100),
+    Picture NVARCHAR(200),
+    Id_restaurant INTEGER,
+    Id_user INTEGER,
+    CONSTRAINT PK_Review PRIMARY KEY (ReviewId),
+    FOREIGN KEY(Id_restaurant) REFERENCES Restaurant (RestaurantId),
+    FOREIGN KEY(Id_user) REFERENCES User(UserId)
 );
 
-CREATE TABLE "driver"
+CREATE TABLE Favorite
 (
-    [Id_driver] INTEGER,
-    FOREIGN KEY([Id_driver]) REFERENCES "user"([Id])  
+    Id_user INTEGER NOT NULL,
+    Id_dish INTEGER NOT NULL,
+    CONSTRAINT PK_Favorite PRIMARY KEY (Id_user,Id_dish),
+    FOREIGN KEY(Id_user) REFERENCES User (UserId),
+    FOREIGN KEY(Id_dish) REFERENCES Dish (DishId)
 );
 
-CREATE TABLE "order"
+CREATE TABLE Driver
 (
-    [Id] INTEGER PRIMARY KEY AUTOINCREMENT,
-    [Id_user] INTEGER,
-    [Id_driver] INTEGER,
-    [State_order] NVARCHAR NOT NULL CHECK(State_order = "received"
-                                         OR State_order = "preparing"
-                                         OR State_order = "ready"
-                                         OR State_order = "delivered"),
-    FOREIGN KEY([Id_user]) REFERENCES "user" ([Id]),
-    FOREIGN KEY([Id_driver]) REFERENCES "driver" ([Id_driver])
+    DriverId INTEGER,
+    FOREIGN KEY(DriverId) REFERENCES User(UserId)  
 );
 
-CREATE TABLE "order_list"
+CREATE TABLE Order_row
 (
-    [Id_order] INTEGER,
-    [Id_dish] INTEGER,
-    PRIMARY KEY(Id_order,Id_dish),
-    FOREIGN KEY([Id_order]) REFERENCES "order" ([Id]),
-    FOREIGN KEY([Id_dish]) REFERENCES "dish" ([Id])
+    OrderId INTEGER NOT NULL,
+    Id_user INTEGER NOT NULL,
+    Id_driver INTEGER NOT NULL,
+    State_order TEXT CHECK(State_order IN ("received","preparing","ready","delivered")),
+    CONSTRAINT PK_Order PRIMARY KEY (OrderId),
+    FOREIGN KEY(Id_user) REFERENCES User (UserId),
+    FOREIGN KEY(Id_driver) REFERENCES Driver (DriverId)
 );
 
-
+CREATE TABLE Order_list
+(
+    Id_order INTEGER,
+    Id_dish INTEGER,
+    CONSTRAINT PK_OrderList PRIMARY KEY(Id_order,Id_dish),
+    FOREIGN KEY(Id_order) REFERENCES Order_row (OrderId),
+    FOREIGN KEY(Id_dish) REFERENCES Dish (DishId)
+);
