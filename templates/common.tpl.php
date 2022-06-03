36d4dcb7e55214b1e9631e1ce2537d2f7661c2c9
Andres Borda <af.borda@uniandes.edu.co>
2022-06-03 17:55:33 +0100

    Common and restaurant templates

---
+++ templates/common.tpl.php
@@ -0,0 +1,46 @@
+<?php declare(trit_types = 1); ?>
+
+<!-- Draws the header of the page -->
+<?php function drawHeader(){ ?>
+
+<!DOCTYPE html>
+
+
+<html lang="en-US">
+        <head>
+            <title>BRZO</title>
+            <meta charset="utf-8">
+            <link rel="stylesheet" href="style.css">
+        </head>
+        <body>
+            <header>
+                <h1><a href="index.php">BRZO</a></h1>
+                <?php drawLoginForm() ?>
+            </header>
+
+            <main>
+        <?php } ?>
+
+
+<!-- Draws the footer of the page -->
+
+<?php function drawFooter() { ?>
+    </main>
+
+    <footer>
+      BRZO &copy; 2022
+    </footer>
+  </body>
+</html>
+<?php } ?>
+
+<!-- Draws the login of the page -->
+
+<?php function drawLoginForm() { ?>
+  <form action="action_login.php" method="post" class="login">
+    <input type="text" name="username" placeholder="username">
+    <input type="password" name="password" placeholder="password">
+    <a href="register.php">Register</a>
+    <button type="submit">Login</button>
+  </form>
+<?php } ?>
---
+++ templates/restaurant.tpl.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types = 1); ?>
+
+<!-- Draws all the restaurants -->
+
+<?php function drawRestaurants(array $restaurants) { ?>
+  <h2>Restaurants</h2>
+  <section id="restaurants">
+    <?php foreach($restaurants as $restaurant) { ?> 
+      <article>
+        <a href="restaurant.php?id=1"><?=$restaurant['name']?></a>
+      </article>
+    <?php } ?>
+  </section>
+<?php } ?>
+
+<!-- Draws the restaurant that is required -->
+
+
+<?php function drawRestaurant(string $restaurantName, array $dishes) { ?>
+  <h2><?=$restaurantName?></h2>
+  <section id="dishes">
+    <?php foreach ($dishes as $dish) { ?>
+    <article>
+      <a href="dish.php?id=1"><?=$dish['name']?></a>
+      <p class="info"><?=$dish['price']?></p>
+    </article>
+    <?php } ?>
+  </section>
+<?php } ?>
