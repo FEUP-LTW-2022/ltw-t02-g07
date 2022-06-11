b90d0f5678c8f4d298b8a225b6f6d62b6ef13d0d
tea <up202111384@up.pt>
2022-06-11 15:02:33 +0100

    css dish menu

--- templates/restaurant.tpl.php
+++ templates/restaurant.tpl.php
@@ -7,9 +7,9 @@
     <div id="part">
       <h2>Restaurants</h2>
       <?php foreach($restaurants as $restaurant) { ?> 
-        <article id="restaurantBox">
-          <img src="../resources/restaurants/<?=$restaurant['picture']?>" id="restaurantImage">
-          <div id ="restaurantDesc">
+        <article id="optionBox">
+          <img src="../resources/restaurants/<?=$restaurant['picture']?>" id="boxImage">
+          <div id="boxDesc">
             <a href="restaurant.php?id=<?=$restaurant['id']?>"><?=$restaurant['name']?></a>
             <p class="info"><?=$restaurant['score']?>★<p>
             <p class="info">Address: <?=$restaurant['address']?><p>
@@ -25,15 +25,20 @@
 
 
 <?php function drawRestaurant(string $restaurantName, array $dishes) { ?>
-  <h2><?=$restaurantName?></h2>
   <section id="dishes">
-    <?php foreach ($dishes as $dish) { ?>
-    <article>
-      <img src="../resources/dishes/<?=$dish['picture']?>">
-      <a href="dish.php?id=<?=$dish['id']?>"><?=$dish['name']?></a>
-      <p class="info"><?=$dish['description']?></p>
-      <p class="info">€<?=$dish['price']?></p>
-    </article>
-    <?php } ?>
+    <div id="part">
+      <h2><?=$restaurantName?></h2>
+      <?php foreach ($dishes as $dish) { ?>
+      <article id="optionBox">
+        <img src="../resources/dishes/<?=$dish['picture']?>" id="boxImage">
+        <div id="boxDesc">
+          <a href="dish.php?id=<?=$dish['id']?>"><?=$dish['name']?></a>
+          <p class="info"><?=$dish['description']?></p>
+          <p class="info">€<?=$dish['price']?></p>
+        </div>
+      </article>
+      <?php } ?>
+      <div id="fix"></div>
+    </div>
   </section>
 <?php } ?>
